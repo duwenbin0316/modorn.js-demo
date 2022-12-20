67351d01de16ae025171575e6aea405bb9e97232
duwenbin0316 <31891889+duwenbin0316@users.noreply.github.com>
2022-12-20 06:29:31 +0000

    更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,57 @@
+import { useEffect, useState } from 'react';
 import { Switch, Route } from '@modern-js/runtime/router';
+import { Table } from 'antd';
+import users from '@api/users';
 
 import './App.css';
 
-const App = () => (
-  <Switch>
-    <Route exact={true} path="/">
-      <div className="container-box">
-        <main>
-          <div className="logo">
-            <img
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ylaelkeh7nuhfnuhf/modernjs-cover.png"
-              width="300"
-              alt="Modern.js Logo"
-            />
-          </div>
-          <p className="description">
-            Get started by editing <code className="code">src/App.tsx</code>
-          </p>
-          <div className="grid">
-            <a href="https://modernjs.dev/docs/start" className="card">
-              <h2>Quick Start</h2>
-            </a>
-            <a href="https://modernjs.dev/docs/guides" className="card">
-              <h2>Handbook</h2>
-            </a>
-            <a href="https://modernjs.dev/docs/apis/app" className="card">
-              <h2>API Reference </h2>
-            </a>
+const App = () => {
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    const load = async () => {
+      const _data = await users();
+
+      setData(_data);
+    };
+
+    load();
+  }, []);
+
+  return (
+    <Switch>
+      <Route exact={true} path="/">
+        <div className="container-box">
+          <main>
+            <div className="logo">
+              <img
+                src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ylaelkeh7nuhfnuhf/modernjs-cover.png"
+                width="300"
+                alt="Modern.js Logo"
+              />
+            </div>
+            <p className="description">
+              Get started by editing{' '}
+              <code className="code">src/App.tsx11xx2</code>
+            </p>
+            <div id="users">{data && <Table dataSource={[]} />}</div>
+          </main>
+          <footer className="footer">
             <a
-              href="https://modernjs.dev/blog"
+              href="https://modernjs.dev"
               target="_blank"
               rel="noopener noreferrer"
-              className="card"
             >
-              <h2>Blog </h2>
+              Powered by Modern.js
             </a>
-          </div>
-        </main>
-        <footer className="footer">
-          <a
-            href="https://modernjs.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by Modern.js asdf
-          </a>
-        </footer>
-      </div>
-    </Route>
-    <Route path="*">
-      <div>404</div>
-    </Route>
-  </Switch>
-);
+          </footer>
+        </div>
+      </Route>
+      <Route path="*">
+        <div>404</div>
+      </Route>
+    </Switch>
+  );
+};
 
 export default App;
